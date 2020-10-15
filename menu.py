f87955d6e2cd0e1876bab2efb479c69ecfd9c7c7
frankjing <zjing@uvm.edu>
2020-10-15 16:39:55 -0400

    Almost finish menu part

--- menu.py
+++ menu.py
@@ -7,10 +7,75 @@ def set_difficulty(value, difficulty):
     difficultyGet[0] = difficulty
 
 def start_the_game():
-    if difficultyGet[0] == 1:
-        print("hard")
-    elif difficultyGet[0] == 2:
-        print("easy")
+    pygame.init()
+
+    # All neccessary attributes
+    nBoxes = 19
+    boxMargin = 1
+    boxWidth = 30
+    boxLength = 30
+    pieceRadius = 8
+    windowWidth = (boxWidth + boxMargin) * nBoxes
+    windowLength = (boxLength + boxMargin) * nBoxes
+    turnOne = True
+
+    pygame.display.set_caption('Gomoku')
+    screen = pygame.display.init()
+    windowSurface = pygame.display.set_mode((windowLength, windowWidth))
+    background = pygame.Surface((windowWidth, windowLength))
+    background.fill(pygame.Color(0,0,0))
+    foreground = pygame.Surface((windowWidth,windowLength))
+
+
+    is_running = True
+
+    while is_running:
+        x = 0
+        y = 0
+        rowDepth = 0
+
+        for i in range(nBoxes * nBoxes):
+            pygame.draw.rect(background, (255,255,255), (x,y,boxWidth, boxLength))
+            x += (boxWidth + boxMargin)
+            rowDepth += 1
+
+            if rowDepth == 19:
+                rowDepth = 0
+                x = 0
+                y = y + (boxLength + boxMargin)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+            if event.type == pygame.KEYDOWN:
+                windowSurface = pygame.display.set_mode((600, 400))
+                menu.mainloop(surface) 
+            # Logic for alternating between black and white pieces
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pieceX, pieceY = pygame.mouse.get_pos()
+
+                # Define piece colours
+                pieceOne = (0,128,128)
+                pieceTwo = (128,0,128)
+
+                # Draw white piece
+                if turnOne:
+                    pygame.draw.circle(foreground, pieceOne, (pieceX,pieceY), pieceRadius)
+                    turnOne = False
+
+                # Draw black piece
+                else:
+                    pygame.draw.circle(foreground, pieceTwo, (pieceX, pieceY), pieceRadius)
+                    turnOne = True
+                #print("Mouse down at (%d, %d)" % event.pos)
+
+
+        windowSurface.blit(background, (0, 0))
+        foreground.set_alpha(100)
+        windowSurface.blit(foreground, (0, 0))
+
+
+        pygame.display.update()
     
 
     
